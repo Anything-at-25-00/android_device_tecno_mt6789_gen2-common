27ad389680f3474c3188b7878821e3269fa77903
Mashopy <eliasgheeraert@gmail.com>
2024-11-04 01:27:28 +0800

    mt6789-common: Fixup media C2 registered version
    Our C2 service target media C2 HIDL 1.2 version, not 1.1
    
    Change-Id: I5adc6bf8a3320acd4b0d991a58f5c8504a3013c3
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -87,6 +87,8 @@ blob_fixups: blob_fixups_user_type = {
      'vendor/lib*/hw/audio.primary.mediatek.so': blob_fixup()
         .add_needed('libstagefright_foundation-v33.so')
         .replace_needed('libalsautils.so, 'libalsautils-v31.so'),
+    'vendor/etc/vintf/manifest/manifest_media_c2_V1_2_default.xml': blob_fixup()
+        .regex_replace('1.1', '1.2'),
 }  # fmt: skip
 
 
