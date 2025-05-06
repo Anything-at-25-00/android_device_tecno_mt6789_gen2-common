3b5617ade6ac337aa5515b689e084099444b6970
=Bowgo Tsai <bowgotsai@google.com>
2025-05-06 15:12:45 +0800

    mt6789-common: Removing GSI keys
    Those keys will be embedded into VtsSecurityAvb on host side
    instead, to verify the GSI image used on the device.
    
    Bug: 149806769
    Test: build and checks those keys are removed from
          $OUT/recovery/root/first_stage_ramdisk/avb/
    Change-Id: I8a002ba6f1421fb460056ccae6572050bdb0ce3c

--- common.mk
+++ common.mk
@@ -191,9 +191,6 @@ PRODUCT_PACKAGES += \
     libshim_sink \
     libshim_ui
 
-# Include GSI keys
-$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
-
 # Init files
 PRODUCT_PACKAGES += \
     fstab.mt6789 \
