75c1f7d686f77867ca7d085ea6a6c846bca96d85
Shirayuki39 <lorddemecrius83@proton.me>
2025-05-25 21:57:05 +0800

    mt6789_gen2-common: Remove Duplicate Entries from the blob list
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -15,7 +15,6 @@ from extract_utils.main import (
 )
 
 blob_fixups: blob_fixups_user_type = {
-
     ('vendor/bin/hw/android.hardware.gnss-service.mediatek', 'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so'): blob_fixup()
         .replace_needed('android.hardware.gnss-V1-ndk_platform.so', 'android.hardware.gnss-V1-ndk.so'),
     ('vendor/lib*/hw/audio.primary.mediatek.so',
