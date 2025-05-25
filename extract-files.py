Strange-F4te/android_device_mt6789-common

mt6789-common: Add vendor soong imports to extract
Change-Id: Ib19e69dc35af6b04519eeb695228af65f3d01d2a
Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

extract-files.py
@@ -14,6 +14,13 @@ from extract_utils.main import (
     ExtractUtilsModule,
 )
 
+namespace_imports = [
+    'device/tecno/mt6789-common',
+    'hardware/mediatek',
+    'hardware/mediatek/libmtkperf_client',
+    'hardware/transsion',
+]
+
 blob_fixups: blob_fixups_user_type = {
     ('vendor/bin/hw/android.hardware.gnss-service.mediatek', 'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so'): blob_fixup()
         .replace_needed('android.hardware.gnss-V1-ndk_platform.so', 'android.hardware.gnss-V1-ndk.so'),
@@ -69,6 +76,7 @@ module = ExtractUtilsModule(
     'mt6789-common',
     'tecno',
     blob_fixups=blob_fixups,
+    namespace_imports=namespace_imports,
 )
 
 if __name__ == '__main__':

Anything-at-25-00/android_device_tecno_mt6789_gen2-common

extract-files.py
@@ -14,6 +14,13 @@ from extract_utils.main import (
     ExtractUtilsModule,
 )
 
+namespace_imports = [
+    'device/transsion/mt6789_gen2-common',
+    'hardware/mediatek',
+    'hardware/mediatek/libmtkperf_client',
+    'hardware/transsion',
+]
+
 blob_fixups: blob_fixups_user_type = {
     ('vendor/bin/hw/android.hardware.gnss-service.mediatek', 'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so'): blob_fixup()
         .replace_needed('android.hardware.gnss-V1-ndk_platform.so', 'android.hardware.gnss-V1-ndk.so'),
@@ -69,6 +76,7 @@ module = ExtractUtilsModule(
     'mt6789_gen2-common',
     'transsion',
     blob_fixups=blob_fixups,
+    namespace_imports=namespace_imports,
 )
 
 if __name__ == '__main__':